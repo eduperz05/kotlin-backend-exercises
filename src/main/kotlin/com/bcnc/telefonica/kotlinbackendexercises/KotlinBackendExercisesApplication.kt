951e3b5f4f54dd47e79175ee2a16c4b9c5bd3944
eduperz05 <eduperz38@gmail.com>
2023-10-29 16:34:16 +0000

    :sparkles: feat: Init Commit

---
+++ src/main/kotlin/com/bcnc/telefonica/kotlinbackendexercises/KotlinBackendExercisesApplication.kt
@@ -0,0 +1,11 @@
+package com.bcnc.telefonica.kotlinbackendexercises
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinBackendExercisesApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinBackendExercisesApplication>(*args)
+}
---
+++ src/test/kotlin/com/bcnc/telefonica/kotlinbackendexercises/KotlinBackendExercisesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bcnc.telefonica.kotlinbackendexercises
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinBackendExercisesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
